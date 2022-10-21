c3ed7e9fafa575a7c6f3f70e6f25a95de3d6eace
xnira01 <muniraseedow@gmail>
2022-10-21 08:00:54 +0200

    Edit README.md and hello_world.txt

---
+++ Avtalen.py
@@ -0,0 +1,10 @@
+#oppgave c 
+class avtale: 
+     def __init__(self, title, sted, starttidspunkt, varighet, kategori):
+         self.tittle= title
+         self.sted= sted
+         self.starttidspunkt = starttidspunkt
+         self.varighet= varighet
+         self.kategori= kategori
+         
+         
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # oving9p1---DATA120
 Avtalebok using Python
+Implementing a simple text-based avtalebok,
+which can register agreements with title, place, 
+start-time and duration. 
