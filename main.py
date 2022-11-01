4eecf739db180d403a27e422b3e949739105a1a2
khaosm <116301862+khaosm@users.noreply.github.com>
2022-11-01 16:27:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -35,3 +35,24 @@ def list_opp_avtaler(liste_med_avtaler, overskrift="ikke satt"):
 list_opp_avtaler(dummy_liste_med_møter)
 
          
+
+
+# oppgave h
+def lagre_liste_m_avtaler(liste):
+    with open ("avtale.txt","w",encoding="UTF-8") as fila:
+        for linje in liste:
+             fila.write(f"{linje}\n")
+#oppgave i
+def les_avtale_fil(filnavn):
+    avtale_liste= list()
+    with open (filnavn,encoding="UTF-8") as fila:
+        for linje in fila:
+            avtale_liste.append(linje)
+    
+#oppgave j
+def dato_i_avtale (avtale_liste,dato):
+    dato_avtale= list()
+    for avtale in avtale_liste:
+        if dato == avtale.starttidspunkt.dato():
+            dato_avtale.append(avtale)
+        return dato_avtale
